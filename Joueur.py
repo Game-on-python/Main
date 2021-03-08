757ab4e752385c6660e29813506cb1b300a52d03
ellganto <79636799+ellganto@users.noreply.github.com>
2021-03-08 12:06:59 +0100

    Update Joueur.py

--- Joueur.py
+++ Joueur.py
@@ -1,2 +1,13 @@
 import pygame
 
+class Joueur(pygame.sprite.Sprite):
+    def __init__(self,x,y,skin,taille,largeur):
+        self.x = x
+        self.y = y
+        self.skin = skin
+        self.taille= taille
+        self.largeur = largeur
+
+
+
+    def movement(self,):
