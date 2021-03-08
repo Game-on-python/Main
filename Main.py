313037b13c8053340d7ce64add4992bdd89c7769
ellganto <79636799+ellganto@users.noreply.github.com>
2021-03-08 11:42:35 +0100

    main and player

---
+++ Main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+# Code RGB du noir et du blanc
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# Taille de l'écran
+SIZE = (700, 500)
+font = pygame.font.Font("BAUHS93.TTF", 40)
+
+
+# Code RGB du noir et du blanc
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# Taille de l'écran
+SIZE = (700, 500)
+font = pygame.font.Font("BAUHS93.TTF", 40)
+
+# Création d'une fenêtre à la taille choisie
+screen = pygame.display.set_mode(SIZE)
